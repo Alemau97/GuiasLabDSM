1d446cd396b1c2e8b990f07805037c2842962f14
Alemau97 <mauriciomn@hotmail.es>
2023-05-11 20:54:55 -0600

    Añadiendo

---
+++ GUIA13/app/src/main/java/sv/edu/udb/guia13/AudioActivity.kt
@@ -0,0 +1,11 @@
+package sv.edu.udb.guia13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio)
+    }
+}
---
+++ GUIA13/app/src/main/java/sv/edu/udb/guia13/TextToSpeechActivity.kt
@@ -0,0 +1,11 @@
+package sv.edu.udb.guia13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextToSpeechActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_to_speech)
+    }
+}
